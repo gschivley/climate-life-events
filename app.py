4892a7a7556137da9106a2de5512039b84565602
gschivley <gschivley@users.noreply.github.com>
2018-02-21 22:47:01 -0500

    Remove merge lines

--- app.py
+++ app.py
@@ -315,12 +315,8 @@ def update_figure(mother_year, self_year, child_year):
             },
             'margin': {
                 'l': 80,
-<<<<<<< HEAD
                 'r': 50,
                 't': 40
-=======
-                'r': 50
->>>>>>> 3d0875b84e3ba6841f361020144fae1b52c2e90d
             },
             'annotations': annotation,
             'hovermode': 'closest',
